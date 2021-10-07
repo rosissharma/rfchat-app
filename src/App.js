d5e75b4354eddd43daac21ed98f61f9eac724e38
rs9110 <rs9110@g.rit.edu>
2021-10-07 19:25:00 -0400

    Update title and favicon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import './index.css';
+import { Helmet } from "react-helmet";
 import ChatRoom from './components/ChatRoom';
 import SignIn from './components/SignIn';
-import {auth} from './firebase.js';
-import {useAuthState} from 'react-firebase-hooks/auth';
+import { auth } from './firebase.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function App() {
+
+  <Helmet>
+    <meta charSet="utf-8" />
+    <title>React Firebase Chat App</title>
+    <meta name="description" content="Chat app built with React and Firebase" />
+  </Helmet>
 
   const [user] = useAuthState(auth);
 
   return (
     <>
-    {user ? <ChatRoom /> : <SignIn />}
+      {user ? <ChatRoom /> : <SignIn />}
     </>
   );
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import firebase from 'firebase'
 import {auth} from '../firebase.js'
+import { Helmet } from 'react-helmet'
 
 function SignIn() {
+    <Helmet>
+        <title>Sign In</title>
+    </Helmet>
+
     function signInWithGoogle() {
         const provider = new firebase.auth.GoogleAuthProvider(); 
         auth.signInWithPopup(provider);
